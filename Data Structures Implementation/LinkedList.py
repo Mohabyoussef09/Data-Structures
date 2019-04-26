f972cb91954586a40e056f48a9571914f3929fc0
Mohab Youssef <mohabyoussef09@gmail.com>
2019-04-26 03:30:23 +0200

    Add files via upload

--- Data Structures Implementation/LinkedList.py
+++ Data Structures Implementation/LinkedList.py
@@ -1,25 +1,97 @@
 class Node:
-    def __init__(self,data):
+     def __init__(self,data):
         self.data=data
         self.next=None
+        
 class LinkedList:
     def __init__(self):
         self.head=None
         self.tail=None
         
-        
     def insert(self,newNode):
-        if self.head is None:
+        if self.head==None:
             self.head=self.tail=newNode
         else:
-            lastnode=self.head
-            while True:
-                if lastnode.next is None:
+            currentNode=self.head
+            while currentNode.next!=None:
+                currentNode=currentNode.next
+            currentNode.next=newNode
+            self.tail=currentNode.next
+
+    def printLinkedList(self):
+        currentNode=self.head
+        while currentNode.next!=None:
+            print(currentNode.data,"-> ",end="")
+            currentNode=currentNode.next
+        print(currentNode.data)
+    def printHead(self):
+        print(self.head.data)
+    def printTail(self):
+        print(self.tail.data)
+        
+        
+    def deleteFromHead(self):
+        temp=self.head
+        self.head=self.head.next
+        del temp
+    def deleteFromTail(self):
+        if self.tail==None:
+            return
+        currentNode=self.head
+        while currentNode.next!=None:
+            PreviousCurrentNode=currentNode
+            currentNode=currentNode.next
+            
+        self.tail=PreviousCurrentNode
+        PreviousCurrentNode.next=None
+        
+    def deleteAt(self,position):
+        currentPosition=0
+        currentNode=self.head
+        if position<0 or position>=self.linkedListLength():
+            print("Invalid input")
+            return
+        if position is 0:
+            self.deleteFromHead()
+            return
+        while True:
+            if currentPosition==position:
+                previousNode.next=currentNode.next
+                del currentNode.next
+                return 
+            
+            else:
+                currentPosition+=1
+                previousNode=currentNode
+                currentNode=currentNode.next
+        
+    def insertFromHead(self,newNode):
+        if self.head==None:
+            self.head=newNode
+        else:
+            temp=self.head
+            self.head=newNode
+            self.head.next=temp
+        
+    def insertBetweenTwoNodes(self,node1,node2,newNode):
+        node1.next=newNode
+        newNode.next=node2
+        
+    def insertBetweenTwoNodesAtPosition(self,position,newNode):
+        if position==0:
+            self.insertFromHead(newNode)
+        else:
+            currentPosition=0
+            currentNode=self.head
+            while currentNode.next!=None:
+                if currentPosition==position:
+                    temp=currentNode.next
+                    currentNode.next=newNode
+                    newNode.next=temp
                     break
-                lastnode=lastnode.next
-            lastnode.next=newNode
-            tail=lastnode
-    
+                else:
+                    currentPosition+=1
+                    currentNode=currentNode.next
     def linkedListLength(self):
         currentNode=self.head
         listLength=0
@@ -27,84 +99,20 @@ class LinkedList:
             listLength+=1
             currentNode=currentNode.next
         return listLength
-        
-            
-    def insertHead(self,newNode):
-        tempNode=self.head
-        self.head=newNode
-        self.head.next=tempNode
-        del tempNode
-        
-    def insertBetweenTwoNodes(self,node1,node2,newNode):
-        node1.next=newNode
-        newNode.next=node2
-         
-        
-    def insertBetweenTwoNodesUsingPosition(self,newNode,pos):
-        currentNode=self.head
-        currentposition=0
-        
-        if pos<0 or pos>self.linkedListLength():
-            print("Invalid input")
-            return
-        if pos is 0:
-            self.insertHead(newNode)
-            return
-        while True:
-            if currentposition==pos:
-                previousNode.next=newNode
-                newNode.next=currentNode
-                break
-            
-            else:
-                previousNode=currentNode
-                currentNode=currentNode.next
-                currentposition+=1
-            
-    def deleteEnd(self):
-        lastNode=self.head
-        while lastNode.next is not None:
-            previousNode=lastNode
-            lastNode=lastNode.next
-        previousNode.next=None
-        
-    def deleteHead(self):
-        t=self.head
-        self.head=self.head.next
-        t.next=None
-        
     
-    def deleteAt(self,pos):
-        currentPos=0
-        currentNode=self.head
-        if pos<0 or pos>=self.linkedListLength():
-            print("Invalid input")
-            return
-        if pos is 0:
-            self.deleteHead()
-            return
-        while True:
-            if currentPos==pos:
-                previousNode.next=currentNode.next
-                del currentNode.next
-                return 
+    def reverse(self):
+        if self.head==None:
+            print("Empty List")
+        if self.head==self.tail:
+            print(self.head.val)
             
-            else:
-                currentPos+=1
-                previousNode=currentNode
-                currentNode=currentNode.next
-                
-    
-    def printLinkedList(self):
-        if self.head is None:
-            print('')
-        currentNode=self.head
-        while True:
-            if currentNode is None:
-                break
-            else:
-                print(currentNode.data)
-                currentNode=currentNode.next
+        else:
+            currentNode=self.head
+            while currentNode.next!=None:
+                #previousNode=currentNode
+                nextNode=currentNode.next
+                currentNode=nextNode.next
+                nextNode.next=currentNode
                 
     def reverse(self): 
         prev = None
@@ -116,58 +124,42 @@ class LinkedList:
             current = next
         self.head = prev
                 
-                
+        
+        
+    
+        
+        
+lk=LinkedList()
+firstN=Node(1)
+secondN=Node(2)
+thirdN=Node(3)
+fourthN=Node(4)
 
-linkedlist=LinkedList()                
-firstNode=Node("FirstNode")
-secondNode=Node("SecondNode")
-thirdNode=Node("ThirdNode")
-
-linkedlist.insert(firstNode)
-linkedlist.insert(secondNode)
-linkedlist.insert(thirdNode)
-
-linkedlist.printLinkedList()
-print("----------------------------")
-NewHeadNode=Node("firstHead")
-linkedlist.insertHead(NewHeadNode)
-linkedlist.printLinkedList()
-
-node_betweet_second_third=Node("between sec and third")
-linkedlist.insertBetweenTwoNodes(secondNode,thirdNode,node_betweet_second_third)
-print("----------------------------")
-linkedlist.printLinkedList()
+lk.insert(firstN)
+lk.insert(secondN)
+lk.insert(thirdN)
+lk.insert(fourthN)
 
 
-node_at_pos1=Node("node at pos 1")
-linkedlist.insertBetweenTwoNodesUsingPosition(node_at_pos1,1)
-print("----------------------------")
-linkedlist.printLinkedList()
+lk.printLinkedList()
+lk.deleteFromTail()
+lk.printLinkedList()
 
-print("----------------------------")
-print(linkedlist.linkedListLength())
+lk.insertBetweenTwoNodes(secondN,thirdN,Node(10))
+lk.printLinkedList()
+lk.insertFromHead(Node(12))
+lk.printLinkedList()
+lk.insertBetweenTwoNodesAtPosition(10,Node(15))
+lk.printLinkedList()
+lk.insertFromHead(Node(20))
+lk.printLinkedList()
+lk.deleteAt(1)
+lk.printLinkedList()
+lk.printTail()
 
+            
+        
 
-linkedlist.deleteEnd()
-print("----------------------------")
-linkedlist.printLinkedList()
+            
+            
 
-
-linkedlist.deleteAt(0)
-print("----------------------------")
-linkedlist.printLinkedList()
-
-
-linkedlist2=LinkedList()  
-firstNode=Node(1)
-secondNode=Node(2)
-thirdNode=Node(3)
-
-linkedlist2.insert(firstNode)
-linkedlist2.insert(secondNode)
-linkedlist2.insert(thirdNode)
-
-linkedlist2.printLinkedList()
-linkedlist2.reverse()
-print("----------------------------")
-linkedlist2.printLinkedList()
